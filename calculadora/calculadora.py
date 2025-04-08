601ace8cff7f40aeef02afdb88427feb9bcb2451
Agatha-Helena <aghata.m@aluno.senai.br>
2025-04-08 08:42:38 -0300

    incluir1
    Alterações no código para melhorar a calculadora.

--- calculadora/calculadora.py
+++ calculadora/calculadora.py
@@ -1,16 +1,16 @@
-
+# -*- coding: UFT-8 -*-
 
 print("Olá! Farei alguns cálculos básicos para você, como soma (+), subtração (-), multiplicação (x) e divisão (/).")
 
 def ad():
     n1 = float(input("Digite o primeiro número: "))
     n2 = float(input("Digite o segundo número: "))
-    return n1 + n2
+    return n1 - n2
 
-
-
-
-
+def pot():
+    n4 = float(input("Digite o primeiro número: "))
+    n1 = int(input("Digite o segundo número: "))
+    return n1 ** n2
 
 def mul():
     n1 = float(input("Digite o primeiro número: "))
@@ -25,13 +25,14 @@ def div():
 def esco():
     if r == "+":
         print(f"O resultado é {ad():.2f}.")
-    
-
+    elif r == "-":
+        print(f"O resultado é {sub():.2f}.")
     elif r == "x" or r == "X":
         print(f"O resultado é {mul():.2f}.")
     elif r == "/":
         print(f"O resultado é {div():.2f}.")
     else:
+        print("Erro")
 
-
-r = input("Escolha o cálculo de acordo com a mensagem acima: ")
+Y = input("Escolha o cálculo de acordo com a mensagem acima: ")
+ad()
